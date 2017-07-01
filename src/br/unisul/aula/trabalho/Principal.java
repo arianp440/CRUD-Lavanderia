14bcf3d3636a7f72a73c2de63c627b8d87f24a55
Maryelen <Maryelen@DESKTOP-E1Q66NA>
2017-07-01 15:54:16 -0300

    Subindo projeto

---
+++ src/br/unisul/aula/trabalho/Principal.java
@@ -0,0 +1,9 @@
+package br.unisul.aula.trabalho;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/br/unisul/aula/trabalho/Roupa.java
@@ -0,0 +1,21 @@
+package br.unisul.aula.trabalho;
+
+public class Roupa {
+	private double valorPeca;
+	
+
+	public void excluirPeca() {
+
+	}
+
+	public void alterarPeca() {
+
+	}
+
+	public double calculaPrecoPagar() {
+		int valor;
+		return valorPeca;
+
+	}
+
+}
